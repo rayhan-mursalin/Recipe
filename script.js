5e10e11e70e63a7f212be7c5604341c4b380bc2c
Rayhan Mursalin <rmursalin@webmd.net>
2021-03-27 16:18:48 -0400

    Get random meals

--- script.js
+++ script.js
@@ -6,3 +6,13 @@ const popupCloseBtn = document.getElementById("close-popup");
 
 const searchTerm = document.getElementById("search-term");
 const searchBtn = document.getElementById("search");
+
+async function getRandomMeal() {
+    const resp = await fetch(
+        "https://www.themealdb.com/api/json/v1/1/random.php"
+    );
+    const respData = await resp.json();
+    const randomMeal = respData.meals[0];
+
+    addMeal(randomMeal, true);
+}

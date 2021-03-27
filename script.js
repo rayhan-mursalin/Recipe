44fb8368f8c772602e61077bb66ee5383d1dce8d
Rayhan Mursalin <rmursalin@webmd.net>
2021-03-27 16:40:52 -0400

    Fetch favorite meals

--- script.js
+++ script.js
@@ -107,3 +107,17 @@ function getMealsLS() {
 
     return mealIds === null ? [] : mealIds;
 }
+
+async function fetchFavMeals() {
+    // clean the container
+    favoriteContainer.innerHTML = "";
+
+    const mealIds = getMealsLS();
+
+    for (let i = 0; i < mealIds.length; i++) {
+        const mealId = mealIds[i];
+        meal = await getMealById(mealId);
+
+        addMealFav(meal);
+    }
+}
